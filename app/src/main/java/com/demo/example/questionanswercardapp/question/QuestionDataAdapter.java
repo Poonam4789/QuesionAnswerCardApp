045c20418048e68de75847aa08950e57d96797dd
Poonam.Patel <poonam.patel@ril.com>
2018-02-27 16:43:36 +0530

    final Build

--- app/src/main/java/com/demo/example/questionanswercardapp/question/QuestionDataAdapter.java
+++ app/src/main/java/com/demo/example/questionanswercardapp/question/QuestionDataAdapter.java
@@ -1,6 +1,9 @@
 package com.demo.example.questionanswercardapp.question;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -10,6 +13,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,6 +21,7 @@ import com.demo.example.questionanswercardapp.R;
 import com.demo.example.questionanswercardapp.answer.AnswerFragment;
 import com.demo.example.questionanswercardapp.model.ResultDataVO;
 
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -31,6 +36,7 @@ public class QuestionDataAdapter extends RecyclerView.Adapter<QuestionDataAdapte
     private Context _context;
     private String quesTitle;
     private String _callActivityName;
+
 
     public QuestionDataAdapter(Context context, String tag, FragmentManager fragmentManager, ArrayList<ResultDataVO> questionList)
     {
@@ -52,6 +58,9 @@ public class QuestionDataAdapter extends RecyclerView.Adapter<QuestionDataAdapte
     {
         quesTitle = _questionsList.get(position).getMessage();
         holder._question.setText(quesTitle);
+        RetrieveImageTask task = new RetrieveImageTask(holder._user_image);
+        task.execute(_questionsList.get(position).getImage());
+
         _commentAdapter = new CommentAdapter(_questionsList.get(position).getCommentsVOArrayList(), _context);
     }
 
@@ -60,7 +69,7 @@ public class QuestionDataAdapter extends RecyclerView.Adapter<QuestionDataAdapte
         private CardView _cardView;
         private TextView _question, _comments, _answers;
         private ListView _listView;
-
+        private ImageView _user_image;
 
         public ViewHolder(View view)
         {
@@ -69,6 +78,8 @@ public class QuestionDataAdapter extends RecyclerView.Adapter<QuestionDataAdapte
             _question = (TextView) view.findViewById(R.id.question);
             _comments = view.findViewById(R.id.comments);
             _answers = view.findViewById(R.id.answers);
+            _user_image = view.findViewById(R.id.user_image);
+
             _listView = view.findViewById(R.id.commentlist);
             _listView.setFastScrollEnabled(true);
 
@@ -123,4 +134,38 @@ public class QuestionDataAdapter extends RecyclerView.Adapter<QuestionDataAdapte
     {
         return _questionsList.size();
     }
+
+    class RetrieveImageTask extends AsyncTask<String, Void, Bitmap>
+    {
+        ImageView _imageView;
+
+        public RetrieveImageTask(ImageView imageView)
+        {
+            _imageView = imageView;
+        }
+
+        protected Bitmap doInBackground(String... urls)
+        {
+            try
+            {
+                URL url = new URL(urls[0]);
+                Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                return bmp;
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap)
+        {
+            super.onPostExecute(bitmap);
+            _imageView.setImageBitmap(bitmap);
+        }
+    }
+
 }
